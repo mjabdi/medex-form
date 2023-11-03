ac52a3957da7f4d56c6d7dbd39f6553091277b00
mjabdi <m.jafarabdi@gmail.com>
2023-11-03 23:31:28 +0330

    form update

--- src/ScreeningPackageLoader.js
+++ src/ScreeningPackageLoader.js
@@ -37,18 +37,36 @@ const packages_health_men_under40 = [
 
   export const categories = [
     { key: "gp", text: "Private Consultation / GP / Doctor", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/gp/"},
-    { key: "gynae", text: "Gynae Clinic", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/gynae/"},
-    // { key: "pcr", text: "PCR TEST", link: "https://travelpcrtest.com/"},
     { key: "std", text: "Sexual Health Clinic", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/std/"},
-    { key: "hs", text: "Health Screening", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=health"},
     { key: "blood", text: "Self Request Tests (Blood, Urine, Swab etc.)", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/blood/"},
-    { key: "visa", text: "Visa Medical", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=visa"},
     { key: "weight_loss", text: "Weight Loss Management", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=weight_loss"},
+    { key: "gynae", text: "Gynae Clinic", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/gynae/"},
+    { key: "hs", text: "Health Screening", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=health"},
+    { key: "visa", text: "Visa Medical", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=visa"},
     { key: "occup", text: "Pre Employment & Occupational Health", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=occupational"},
     { key: "allergy", text: "Allergy Screening", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=allergy", small: false},
     { key: "corp", text: "Corporate Clients", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/corporate/", small: false},
-
   ]
+
+
+  export const categoriesWalkin = [
+    { key: "gp", text: "Private Consultation / GP / Doctor", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/gp/"},
+    { key: "std", text: "Sexual Health Clinic", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/std/"},
+    { key: "blood", text: "Self Request Tests (Blood, Urine, Swab etc.)", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/blood/"},
+    { key: "weight_loss", text: "Weight Loss Management", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=weight_loss"},
+  ]
+
+  export const categoriesAppointment = [
+    { key: "gynae", text: "Gynae Clinic", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/gynae/"},
+    { key: "hs", text: "Health Screening", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=health"},
+    { key: "visa", text: "Visa Medical", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=visa"},
+    { key: "occup", text: "Pre Employment & Occupational Health", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=occupational"},
+    { key: "allergy", text: "Allergy Screening", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/screening/?cat=allergy", small: false},
+    { key: "corp", text: "Corporate Clients", link: "https://londonmedicalclinic.co.uk/medicalexpressclinic/book/corporate/", small: false},
+  ]
+
+
+
 
   export const getCatText = (key) => {
       const cat = categories.find(e => e.key === key)
--- src/WelcomeForm.js
+++ src/WelcomeForm.js
@@ -1,72 +1,68 @@
-import React, { useEffect } from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
-import GlobalState from './GlobalState';
+import React, { useEffect } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import GlobalState from "./GlobalState";
 
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
-import HttpsIcon from '@material-ui/icons/Https';
+import HttpsIcon from "@material-ui/icons/Https";
 
-import {BrowserView, MobileView, isMobile} from 'react-device-detect';
+import { BrowserView, MobileView, isMobile } from "react-device-detect";
 
-import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
-import { Grid } from '@material-ui/core';
+import AirplanemodeActiveIcon from "@material-ui/icons/AirplanemodeActive";
+import { Grid } from "@material-ui/core";
 
-import logoImage from './images/logo.png';
-import doctorImage from './images/doctor.png';
+import logoImage from "./images/logo.png";
+import doctorImage from "./images/doctor.png";
 
-import LiveHelpIcon from '@material-ui/icons/LiveHelp';
-import faq from './FAQ';
-import SmartEntryForm from './SmartEntryForm';
-import CategoriesForm from './CategoriesForm';
-
+import LiveHelpIcon from "@material-ui/icons/LiveHelp";
+import faq from "./FAQ";
+import SmartEntryForm from "./SmartEntryForm";
+import CategoriesFormWalkin from "./CategoriesFormWalkin";
+import CategoriesFormAppointment from "./CategoriesFormAppointment";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      {new Date().getFullYear()}
-      {' '}
+      {"Copyright © "}
+      {new Date().getFullYear()}{" "}
       <Link color="inherit" href="#">
-           <strong> Medical Express Clinic </strong> 
-      </Link>{isMobile ? ' ' : ' All rights reserved.' }
-   
-       
- 
-     
+        <strong> Medical Express Clinic </strong>
+      </Link>
+      {isMobile ? " " : " All rights reserved."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    position: 'relative',
+    position: "relative",
     backgroundColor: "#f68529",
     color: "#fff",
-    alignItems: 'center',
+    alignItems: "center",
   },
 
   logo: {
     maxWidth: 160,
   },
   layout: {
-    width: 'auto',
+    width: "auto",
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
       width: 700,
-      marginLeft: 'auto',
-      marginRight: 'auto',
+      marginLeft: "auto",
+      marginRight: "auto",
     },
   },
   paper: {
@@ -83,8 +79,8 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3, 0, 5),
   },
   buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end",
   },
   button: {
     marginTop: theme.spacing(3),
@@ -93,13 +89,13 @@ const useStyles = makeStyles((theme) => ({
 
   bold: {
     fontWeight: "800",
-    padding: "5px"
+    padding: "5px",
   },
 
   doneImage: {
     width: "240px",
     height: "150px",
-    margin: "20px"
+    margin: "20px",
   },
 
   logoImage: {
@@ -115,89 +111,104 @@ const useStyles = makeStyles((theme) => ({
   },
 
   privacyButton: {
-    marginBottom : "20px",
-    width: "115px"
+    marginBottom: "20px",
+    width: "115px",
   },
 
   faqButton: {
-    marginBottom : "20px",
-    marginLeft : "10px",
+    marginBottom: "20px",
+    marginLeft: "10px",
     // backgroundColor : "#2f942e",
     // "&:hover": {
     //   background: "green",
     //   color: "#fff"
     // },
-    textDecoration : "none !important",
-    width: "115px"
-
+    textDecoration: "none !important",
+    width: "115px",
   },
 
-  textContent : {
-      color : "#666f77",
-      fontSize : "1.1rem",
-      textAlign: "left",
-      paddingLeft: "30px",
-      paddingRight: "30px",
-      lineHeight: "2.2em",
-      fontWeight : "400"
-  },
-
-  textContentMobile : {
-    color : "#666f77",
-    fontSize : "0.9rem",
+  textContent: {
+    color: "#666f77",
+    fontSize: "1.1rem",
     textAlign: "left",
     paddingLeft: "30px",
     paddingRight: "30px",
     lineHeight: "2.2em",
-    fontWeight : "400"
-},
+    fontWeight: "400",
+  },
+
+  textContentMobile: {
+    color: "#666f77",
+    fontSize: "0.9rem",
+    textAlign: "left",
+    paddingLeft: "30px",
+    paddingRight: "30px",
+    lineHeight: "2.2em",
+    fontWeight: "400",
+  },
 
   getStartedButton: {
-      marginTop : "10px",
-      marginBottom : "10px",
-
+    marginTop: "10px",
+    marginBottom: "10px",
   },
 
-  AirIcon : {
-      marginRight : "10px",
-      fontSize: "32px"
+  AirIcon: {
+    marginRight: "10px",
+    fontSize: "32px",
   },
 
-  pageTitle:{
-    color : theme.palette.primary.main,
-    marginTop : "10px"
+  pageTitle: {
+    color: theme.palette.primary.main,
+    marginTop: "10px",
   },
 
   Treatment: {
     paddingTop: "20px",
     fontSize: "1.5rem",
     fontWeight: "500",
-    color : "#555"
+    color: "#555",
+  },
 
-  }
+  BookNow: {
+    paddingBottom: "10px",
+    paddingTop: "0px",
+    fontSize: "1.2rem",
+    fontWeight: "600",
+    color: "#ed5f00",
+  },
 
+  BookNowAfter: {
+    paddingTop: "8px",
+    fontSize: "1rem",
+    fontWeight: "600",
+    color: "#9e3302",
+  },
+
+  ByAppointment: {
+    paddingBottom: "10px",
+    paddingTop: "0px",
+    fontSize: "1.2rem",
+    fontWeight: "600",
+    color: "#ed5f00",
+  },
 }));
-
-
-
 
 export default function WelcomeForm() {
   const [state, setState] = React.useContext(GlobalState);
   const classes = useStyles();
 
-
   //// ** Dialog
 
   const [open, setOpen] = React.useState(false);
-  const [scroll, setScroll] = React.useState('paper');
+  const [scroll, setScroll] = React.useState("paper");
 
   const [openFAQ, setOpenFAQ] = React.useState(false);
-  const [scrollFAQ, setScrollFAQ] = React.useState('paper');
+  const [scrollFAQ, setScrollFAQ] = React.useState("paper");
 
   const descriptionElementRef = React.useRef(null);
 
   useEffect(() => {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }, []);
 
   React.useEffect(() => {
@@ -219,7 +230,6 @@ export default function WelcomeForm() {
     }
   }, [openFAQ]);
 
-
   const handleClickOpen = (scrollType) => () => {
     setOpen(true);
     setScroll(scrollType);
@@ -238,10 +248,9 @@ export default function WelcomeForm() {
     setOpenFAQ(false);
   };
 
-
-const getStartedClicked = (event) => {
-    setState(state => ({...state, getStarted: true}));
-}
+  const getStartedClicked = (event) => {
+    setState((state) => ({ ...state, getStarted: true }));
+  };
 
   return (
     <React.Fragment>
@@ -265,22 +274,30 @@ const getStartedClicked = (event) => {
                 Book Appointment Online
               </Typography>
             </Grid>
-
-
           </Grid>
         </Toolbar>
       </AppBar>
 
-      <div className={classes.Treatment}>
-        Medical Express Clinic Treatments
-      </div>
+      <div className={classes.Treatment}>Medical Express Clinic Treatments</div>
+
+      {/* <div className={classes.BookNowAfter}>Private consultation, sexual health, self request, weight loss</div> */}
 
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <CategoriesForm/>
+          <div className={classes.BookNow}>Book now or Walk in</div>
+
+          <CategoriesFormWalkin />
         </Paper>
 
-       <Copyright />
+        <Paper className={classes.paper}>
+          <div className={classes.ByAppointment}>
+            By appointment only All the rest
+          </div>
+
+          <CategoriesFormAppointment />
+        </Paper>
+
+        <Copyright />
       </main>
     </React.Fragment>
   );
